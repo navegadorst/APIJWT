c13f6318befcdc5c78638eaf1211eae82b8ec665
Torres Espinoza Alejandro <alejandro.torres@scanda.com.mx>
2019-06-14 01:53:47 -0500

    send cahnge

--- ApiLogin/Controllers/TokenGen.cs
+++ ApiLogin/Controllers/TokenGen.cs
@@ -13,7 +13,7 @@ namespace ApiLogin.Controllers
     {
         public static string GenerateTokenJwt(string username)
         {
-            // appsetting for Token JWT
+           
             var ctkn = new ConfigurationTocken();
             var secretKey = ctkn.SecretKey;
             var audienceToken = ctkn.host;
@@ -23,10 +23,10 @@ namespace ApiLogin.Controllers
             var securityKey = new SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(secretKey));
             var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
 
-            // create a claimsIdentity
+            
             ClaimsIdentity claimsIdentity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, username) });
 
-            // create token to the user
+ 
             var tokenHandler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
             var jwtSecurityToken = tokenHandler.CreateJwtSecurityToken(
                 audience: audienceToken,
